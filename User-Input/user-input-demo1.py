59c20e98da5b7caf1720cd003fb32ca7ce638aa3
easy2-code <97843826+easy2-code@users.noreply.github.com>
2023-09-24 22:56:24 +0500

    Python User Input

---
+++ User-Input/user-input-demo1.py
@@ -0,0 +1,8 @@
+# programs commonly need to read input typed by the user on the keyboard.
+# We will use the Python functions to do this
+# Get the user's first name
+Fname=input("Enter your fisrt name: ")
+# Get the user's last name
+Lname=input("Enter your last name: ")
+# Now print the name
+print("Hello",Fname,Lname)
---
+++ User-Input/user-input-demo2.py
@@ -0,0 +1,14 @@
+# programs commonly need to read input typed by the user on the keyboard.
+# We will use the Python functions to do this
+# Reading Numbers with the input Function
+# int, float ......etc
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+income=float(input("Enter your income: "))
+
+# Display the data
+print("Here is the data you entered")
+print("==============================")
+print("Name: ",name)
+print("Age: ",age)
+print("Income: ",income)
