8be512da3f381a0806032ddfb4e8f945442c6c55
easy2-code <97843826+easy2-code@users.noreply.github.com>
2023-09-24 22:56:41 +0500

    Performing Calculations

---
+++ Performing-Calculation/Addition.py
@@ -0,0 +1,6 @@
+# Take input from user
+number_1=int(input("Enter First Number: "))
+number_2=int(input("Enter Second Number: "))
+# + operator user to add two numbers
+addition=number_1+number_2
+print("The addition of",number_1,"and",number_2,"=",addition)
---
+++ Performing-Calculation/Subtraction.py
@@ -0,0 +1,5 @@
+# Take input from user
+number_1=int(input("Enter number: "))
+number_2=int(input("Enter number: "))
+subtraction=number_1-number_2
+print("The subtraction: ",subtraction)
